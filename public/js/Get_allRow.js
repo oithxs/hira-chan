46fa3713a33e2d14380bce4a1124085e96ae5984
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-04-29 07:23:05 +0000

    Laravel Mix使用

--- public/js/Get_allRow.js
+++ public/js/Get_allRow.js
@@ -1,29 +1,38 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************************!*\
+  !*** ./resources/js/Get_allRow.js ***!
+  \************************************/
 display();
 setInterval('display()', 1000);
 
 function display() {
-    var displayArea = document.getElementById("displayArea");
-    
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
+  var displayArea = document.getElementById("displayArea");
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
+  $.ajax({
+    type: "POST",
+    url: url + "/jQuery.ajax/getRow",
+    data: {
+      'table': table
+    },
+    dataType: "json"
+  }).done(function (data) {
+    // 成功時
+    displayArea.innerHTML = "<br>";
 
-    $.ajax({
-        type: "POST",
-        url: url + "/jQuery.ajax/getRow",
-        data: { 'table': table },
-        dataType: "json",
-    }).done(function (data) { // 成功時
-        displayArea.innerHTML = "<br>";
-        for (var item in data) {
-            displayArea.insertAdjacentHTML("afterbegin", data[item]['no'] + ": " + data[item]['name'] + " " + data[item]['time'] + "<br>" +
-                data[item]['message'] + "<hr><br>");
-        }
-    }).fail(function (XMLHttpRequest, textStatus, errorThrown) { // 失敗時
-        console.log(XMLHttpRequest.status);
-        console.log(textStatus);
-        console.log(errorThrown.message);
-    });
+    for (var item in data) {
+      displayArea.insertAdjacentHTML("afterbegin", data[item]['no'] + ": " + data[item]['name'] + " " + data[item]['time'] + "<br>" + data[item]['message'] + "<hr><br>");
+    }
+  }).fail(function (XMLHttpRequest, textStatus, errorThrown) {
+    // 失敗時
+    console.log(XMLHttpRequest.status);
+    console.log(textStatus);
+    console.log(errorThrown.message);
+  });
 }
+/******/ })()
+;
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/Get_allRow.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
