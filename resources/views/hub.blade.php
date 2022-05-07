5801cdaf2b56c4c977c539802f7c9e7bd9070630
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-05-07 23:38:21 +0900

    bootstrap読み込み・jsを全て下に

--- resources/views/hub.blade.php
+++ resources/views/hub.blade.php
@@ -1,42 +1,67 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{__('Forum Hub')}}
-        </h2>
-    </x-slot>
+	<x-slot name="header">
+		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+			{{__('Forum Hub')}}
+		</h2>
+	</x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+	<div class="py-12">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
 			<!-- my area begin -->
-            <div>
-                <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
-                <script>
-                    const url = "{{$url}}";
-                </script>
-            </div>
-            <div>
-                <form id="createThread">
-                    <p>{{__('Thread name')}}<input type="text" name="threadName"></P>
-                </form>
-                <button id="create_threadBtn">{{__('Create new thread')}}</button>
-                <script src="{{ mix('js/Create_thread.js') }}"></script>
-            </div>
+			<head>
+				<meta charset="utf-8">
+				<meta name="csrf-token" content="{{ csrf_token() }}">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<!-- Bootstrap CSS -->
+				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+				rel="stylesheet" 
+				integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+				crossorigin="anonymous">
+			</head>
+			<body>
+				<div>
+					<form id="createThread">
+						<p>{{__('Thread name')}}<input type="text" name="threadName"></P>
+					</form>
+					<button id="create_threadBtn">{{__('Create new thread')}}</button>
+				</div>
 
-            <br><br>
-            
-            <div>
-                <table>
-                    <tr><th>{{__('Thread name')}}</th><td>{{__('Create time')}}</td></tr>
-                    @foreach($tables as $tableInfo)
-                        <tr><th><a href="keiziban?table%5B%5D={{$tableInfo['table_name']}}">{{$tableInfo['table_name']}}</a></th><td>{{$tableInfo['created_at']}}</td></tr>
-                    @endforeach
-                </table>
-            </div>
-			<!-- my area begin -->
+				<br><br>
+				
+				<div>
+					<table>
+						<tr><th>{{__('Thread name')}}</th><td>{{__('Create time')}}</td></tr>
+						@foreach($tables as $tableInfo)
+							<tr><th><a href="keiziban?table%5B%5D={{$tableInfo['table_name']}}">{{$tableInfo['table_name']}}</a></th><td>{{$tableInfo['created_at']}}</td></tr>
+						@endforeach
+					</table>
+				</div>
+
+				<div>
+					<!-- Bootstrap用JavaScript -->
+					<script
+					src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+					integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+					crossorigin="anonymous"
+					></script>
+
+					<!-- jQuery -->
+					<script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+
+					<!-- グローバル変数 -->
+					<script>
+						const url = "{{$url}}";
+					</script>
+
+					<!-- others -->
+					<script src="{{ mix('js/Create_thread.js') }}"></script>
+				</div>
+			</body>
+			<!-- my area end -->
 			
-            </div>
-        </div>
-    </div>
+			</div>
+		</div>
+	</div>
 </x-app-layout>
--- resources/views/keiziban.blade.php
+++ resources/views/keiziban.blade.php
@@ -10,16 +10,18 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
 			<!-- my area begin -->
-				<div>
+				<head>
+					<meta charset="utf-8">
 					<meta name="csrf-token" content="{{ csrf_token() }}">
-					<script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
-					<script>
-						const url = "{{$url}}";
-						const table = "{{$tableName}}";
-					</script>
-				</div>
+					<meta name="viewport" content="width=device-width, initial-scale=1">
+					<!-- Bootstrap CSS -->
+					<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+					rel="stylesheet" 
+					integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+					crossorigin="anonymous">
+				</head>
 
-				<div>					
+				<body>
 					<a href="{{ url('/hub') }}">{{__('Go hub')}}</a>
 					<br><br>
 					
@@ -31,15 +33,34 @@
 							<textarea name="message"></textarea>
 						</form>
 						<button id="sendMessageBtn">{{__('Write forum')}}</button>
-						<script src="{{ mix('js/Send_Row.js') }}"></script>
 					</div>
 					
 					<br><br><br>
 					
-					<div id="displayArea">
+					<div id="displayArea"></div>
+
+					<div>
+						<!-- Bootstrap用JavaScript -->
+						<script
+						src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+						integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+						crossorigin="anonymous"
+						></script>
+
+						<!-- jQuery -->
+						<script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+
+						<!-- グローバル変数 -->
+						<script>
+							const url = "{{$url}}";
+							const table = "{{$tableName}}";
+						</script>
+
+						<!-- others -->
+						<script src="{{ mix('js/Send_Row.js') }}"></script>
 						<script src="{{ mix('/js/Get_allRow.js') }}"></script>
 					</div>
-				</div>
+				</body>
 			<!-- my area end -->
 
             </div>
