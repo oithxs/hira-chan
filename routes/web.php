4f68429ab3b8c27b832d3a686eeedb9ab6ff1ff6
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-05-07 17:34:09 +0900

    ルートの書き方調整

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function() { return view('welcome');});
+
+// データ処理
 Route::post('jQuery.ajax/getRow', "App\Http\Controllers\jQuery_ajax@get_allRow");
 Route::post('jQuery.ajax/sendRow', "App\Http\Controllers\jQuery_ajax@send_Row");
 Route::post('jQuery.ajax/create_thread', "App\Http\Controllers\jQuery_ajax@create_thread");
+
+// 画面遷移
+Route::get('/', function() { return view('welcome');});
 Route::get('/account/delete/{id}/{hash}', 'App\Http\Controllers\AccountDelete')->middleware('auth')->name('account/delete');
 
 Route::middleware([
@@ -23,23 +27,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
+    Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
     Route::get('/hub', 'App\Http\Controllers\showTablesCTL')->name('hub');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
     Route::get('/keiziban', 'App\Http\Controllers\keizibanCTL')->name('keiziban');
 });
