ad693fd447ead97f54b11fb610728e1f34884705
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-05-12 10:02:48 +0900

    入力されたスレッド名ではなくUUIDでテーブルを作成(send・getはまだ)

--- app/Http/Controllers/keizibanCTL.php
+++ app/Http/Controllers/keizibanCTL.php
@@ -8,9 +8,9 @@ use App\Models\Get;
 class keizibanCTL extends Controller {
 	public function __invoke(Request $request) {
 		$get = new Get;
-		$stmt = $get->allRow($request->table);
+		$stmt = $get->allRow($request->thread_id);
 
-		$response['tableName'] = $request->table;
+		$response['thread_name'] = $request->thread_name;
 		$response['thread_id'] = $request->thread_id;
 		$response['username'] = $request->user()->name;
 		$response['url'] = url('/');
--- app/Models/create_thread.php
+++ app/Models/create_thread.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
 class create_thread extends Model {
-    public function create_thread($tableName) {
-        Schema::connection('mysql_keiziban')->create($tableName, function (Blueprint $table) {
+    public function create_thread($thread_id) {
+        Schema::connection('mysql_keiziban')->create($thread_id, function (Blueprint $table) {
             $table->id('no');
             $table->text('name');
             $table->text('message');
@@ -18,10 +18,10 @@ class create_thread extends Model {
         return null;
     }
 
-    public function insertTable($tableName) {
+    public function insertTable($tableName, $thread_id) {
         DB::connection('mysql_keiziban')->insert(
-        "INSERT INTO hub(id, table_name, created_at) VALUES(NULL, :table_name, NOW())", 
-        [$tableName]);
+        "INSERT INTO hub(id, thread_id, thread_name, created_at) VALUES(NULL, :thread_id, :thread_name, NOW())", 
+        [$thread_id, $tableName]);
         return null;
     }
 }
