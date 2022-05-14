826ef58d15ed7c437eea41fb7e1bbdb7b8309ed2
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-05-14 22:09:31 +0900

    ログイン時のパスワード入力欄に注意喚起メッセージを追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="{{ __('Password') }}{{ __('Password login notice') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
