c75381e2bf057d4bb11a69b3966103f31599df23
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-05-14 22:10:19 +0900

    パスワード変更用のformに注意喚起メッセージを追加

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -15,7 +15,7 @@
         </div>
 
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="password" value="{{ __('New Password') }}" />
+            <x-jet-label for="password" value="{{ __('New Password') }}{{ __('Password register notice') }}" />
             <x-jet-input id="password" type="password" class="mt-1 block w-full" wire:model.defer="state.password" autocomplete="new-password" />
             <x-jet-input-error for="password" class="mt-2" />
         </div>
