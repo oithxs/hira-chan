40c6b3a0c26d8ec07a70a87a89b6147555e72820
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-05-15 21:52:29 +0900

    スーパユーザから個人ユーザに切り替え(メール送信以前はおそらく完成)

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]; then
-	:
-else 
-	echo "スーパユーザでは実行しないで下さい"
+	echo "Please run as root."
 	exit 1
 fi
 
 # 入力が必要な物を最初に
 
-read -sp "[sudo] password for $USER: " sudo_pass
+echo -n "個人ユーザ名："
+read personal_user
 echo "データベースを2つ作成します．データベース名を入力してください"
 echo "※「_」は使用可能ですが，「-」は使用できません"
 echo -n "メインデータベース："
@@ -20,37 +19,37 @@ echo "MySQL/phpMyAdmin関連のパスワードを設定します"
 read -sp "Password: " mysql_pass
 echo 
 
-cd
+cd /home/$personal_user
 
 # 必要なパッケージを一括インストール
 
-sudo apt update -y
-sudo apt install -y net-tools expect php libapache2-mod-php php-gd php-xml php-cli php-mbstring php-soap php-xmlrpc php-zip nodejs npm php-mysql
+apt update -y
+apt install -y net-tools expect php libapache2-mod-php php-gd php-xml php-cli php-mbstring php-soap php-xmlrpc php-zip nodejs npm php-mysql
 
 # LAMPPのインストール
 
 wget https://www.apachefriends.org/xampp-files/8.1.5/xampp-linux-x64-8.1.5-0-installer.run
 chmod +x xampp-linux-x64-8.1.5-0-installer.run
-yes | sudo ./xampp-linux-x64-8.1.5-0-installer.run
+yes | ./xampp-linux-x64-8.1.5-0-installer.run
 
 # apache2を停止
 
-sudo systemctl disable apache2
-sudo systemctl stop apache2.service
+systemctl disable apache2
+systemctl stop apache2.service
 
 # phpMyAdminを使用可能に
 
-sudo sed -ie 's/Require local/Require all granted/' /opt/lampp/etc/extra/httpd-xampp.conf >/dev/null 2>&1
+sed -ie 's/Require local/Require all granted/' /opt/lampp/etc/extra/httpd-xampp.conf >/dev/null 2>&1
 
 # LAMPP起動時のエラーを解決
 
-sudo sed -ie '/UserPassword daemon <?/,/?>/s|^|#|' /opt/lampp/etc/proftpd.conf >/dev/null 2>&1
+sed -ie '/UserPassword daemon <?/,/?>/s|^|#|' /opt/lampp/etc/proftpd.conf >/dev/null 2>&1
 
 # LAMPPを起動
 
-sudo /opt/lampp/lampp stop
-sudo /opt/lampp/lampp start
-sudo /opt/lampp/lampp restart
+/opt/lampp/lampp stop
+/opt/lampp/lampp start
+/opt/lampp/lampp restart
 
 # データベースの作成
 
@@ -63,8 +62,6 @@ command="/opt/lampp/lampp security"
 
 expect -c "
 	spawn $command
-	expect \"password\"
-	send \"$sudo_pass\n\"
 	expect \"yes\"
 	send \"no\n\"
 	expect \"yes\"
@@ -91,29 +88,32 @@ expect -c "
 # Composer のインストール
 
 curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-sudo chmod +x /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
 source ~/.bashrc
 
 # node.js関連のインストール
 
-sudo npm install n -g
-sudo n lts
+npm install n -g
+n lts
 
 # リポジトリのクローン
 
-sudo mv Laravel_Forum-B /opt/lampp/htdocs
+mv Laravel_Forum-B /opt/lampp/htdocs
 cd /opt/lampp/htdocs/Laravel_Forum-B
 
 # ドキュメントルート変更
 
-sudo sed -ie 's/DocumentRoot "\/opt\/lampp\/htdocs"/DocumentRoot "\/opt\/lampp\/htdocs\/Laravel_Forum-B\/public"/' /opt/lampp/etc/httpd.conf
-sudo sed -ie 's/<Directory "\/opt\/lampp\/htdocs">/<Directory "\/opt\/lampp\/htdocs\/Laravel_Forum-B\/public">/' /opt/lampp/etc/httpd.conf
+sed -ie 's/DocumentRoot "\/opt\/lampp\/htdocs"/DocumentRoot "\/opt\/lampp\/htdocs\/Laravel_Forum-B\/public"/' /opt/lampp/etc/httpd.conf
+sed -ie 's/<Directory "\/opt\/lampp\/htdocs">/<Directory "\/opt\/lampp\/htdocs\/Laravel_Forum-B\/public">/' /opt/lampp/etc/httpd.conf
 
 # リポジトリの公開準備
 
+sudo su - $personal_user <<EOF
+cd /opt/lampp/htdocs/Laravel_Forum-B
 composer install
 npm install
+EOF
 
 cp .env.example .env
 php artisan key:generate
@@ -124,10 +124,10 @@ sed -ie "s/DB_PASSWORD=/DB_PASSWORD=$mysql_pass/" .env
 
 chmod 777 -R storage
 
-sudo php artisan migrate
+php artisan migrate
 
 # LAMPP の起動
 
-sudo /opt/lampp/lampp restart
+/opt/lampp/lampp restart
 
 exit 0
