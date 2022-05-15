47a7a595c810c9fc583a443251c932a66325c895
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-05-16 00:14:25 +0900

    カラムを追加・スレッド作成機能再生

--- database/migrations/2022_05_05_112244_create_hub_table.php
+++ database/migrations/2022_05_05_112244_create_hub_table.php
@@ -15,7 +15,8 @@ class CreateHubTable extends Migration
     {
         Schema::connection('mysql_keiziban')->create('hub', function (Blueprint $table) {
             $table->id();
-            $table->string('table_name');
+            $table->string('thread_id');
+            $table->string('thread_name');
             $table->string('created_at');
         });
     }
