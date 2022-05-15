274174831d85f7f9fd2e4d425d3d95e7bc7e7f7c
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-05-15 23:44:41 +0900

    メールインストール以外自動化・ドキュメントの追加

--- setup.sh
+++ setup.sh
@@ -5,10 +5,18 @@ if [ "$EUID" -ne 0 ]; then
 	exit 1
 fi
 
+ls /etc | grep postfix >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "Postfix is not installed"
+	exit 1
+fi
+
 # 入力が必要な物を最初に
 
 echo -n "個人ユーザ名："
 read personal_user
+echo -n "アプリ名："
+read Application_name
 echo "データベースを2つ作成します．データベース名を入力してください"
 echo "※「_」は使用可能ですが，「-」は使用できません"
 echo -n "メインデータベース："
@@ -18,6 +26,11 @@ read keiziban_database
 echo "MySQL/phpMyAdmin関連のパスワードを設定します"
 read -sp "Password: " mysql_pass
 echo 
+echo "メール送信関連の設定をします"
+echo -n "アカウント名(@以下不要)："
+read account_name
+read -sp "Application Password: " Application_pass
+echo
 
 cd /home/$personal_user
 
@@ -47,9 +60,9 @@ sed -ie '/UserPassword daemon <?/,/?>/s|^|#|' /opt/lampp/etc/proftpd.conf >/dev/
 
 # LAMPPを起動
 
-/opt/lampp/lampp stop
-/opt/lampp/lampp start
-/opt/lampp/lampp restart
+/opt/lampp/lampp stop >/dev/null 2>&1
+/opt/lampp/lampp start >/dev/null 2>&1
+/opt/lampp/lampp restart >/dev/null 2>&1
 
 # データベースの作成
 
@@ -97,7 +110,7 @@ source ~/.bashrc
 npm install n -g
 n lts
 
-# リポジトリのクローン
+# リポジトリの移動
 
 mv Laravel_Forum-B /opt/lampp/htdocs
 cd /opt/lampp/htdocs/Laravel_Forum-B
@@ -116,8 +129,11 @@ npm install
 EOF
 
 cp .env.example .env
+chmod g+w .env
+chown $personal_user:$personal_user .env
 php artisan key:generate
 
+sed -ie "s/APP_NAME=Laravel/APP_NAME=$Application_name/" .env
 sed -ie "s/DB_DATABASE=laravel/DB_DATABASE=$main_database/" .env
 sed -ie "s/DB_DATABASE_KEIZIBAN=/DB_DATABASE_KEIZIBAN=$keiziban_database/" .env
 sed -ie "s/DB_PASSWORD=/DB_PASSWORD=$mysql_pass/" .env
@@ -126,8 +142,44 @@ chmod 777 -R storage
 
 php artisan migrate
 
+# メールアプリパスワードの適応
+
+cd /etc/postfix
+echo "[smtp.gmail.com]:587 $account_name@gmail.com:$Application_pass" > sasl_passwd
+chmod 600 sasl_passwd
+postmap /etc/postfix/sasl_passwd
+
+# main.cfの編集
+
+cat <<EOF >>main.cf
+	smtp_use_tls = yes
+	smtp_sasl_auth_enable = yes
+	smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
+	smtp_sasl_tls_security_options = noanonymous
+	inet_protcols=ipv4
+EOF
+
+# 設定の反映
+
+systemctl restart postfix
+
+# php.iniの編集
+
+sed -ie 's/;sendmail_path =/sendmail_path = \/usr\/sbin\/sendmail -t -i/' /opt/lampp/etc/php.ini
+
+# .envファイルの編集
+
+cd /opt/lampp/htdocs/Laravel_Forum-B
+sed -ie 's/MAIL_HOST=mailhog/MAIL_HOST=smtp.gmail.com/' .env
+sed -ie 's/MAIL_PORT=1025/MAIL_PORT=587/' .env
+sed -ie "s/MAIL_USERNAME=null/MAIL_USERNAME=$account_name@gmail.com/" .env
+sed -ie "s/MAIL_PASSWORD=null/MAIL_PASSWORD=$Application_pass/" .env
+sed -ie 's/MAIL_ENCRYPTION=null/MAIL_ENCRYPTION=tls/' .env
+sed -ie "s/MAIL_FROM_ADDRESS=null/MAIL_FROM_ADDRESS=$account_name@gmail.com/" .env
+
 # LAMPP の起動
 
+rm .enve
 /opt/lampp/lampp restart
 
 exit 0
