9520da9f6ca135fa5166490d5d9d293f1f6e7ef9
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-05-15 15:46:32 +0900

    スーパユーザで動かない箇所があったのでスーパユーザでは実行できない様に

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# めんどくさいのでスーパユーザか確認
-
 if [ "$EUID" -ne 0 ]; then
-	echo "Please run as root"
+	:
+else 
+	echo "スーパユーザでは実行しないで下さい"
 	exit 1
 fi
 
@@ -20,61 +20,60 @@ cd
 
 # 必要なパッケージを一括インストール
 
-apt update -y
-apt install -y net-tools php libapache2-mod-php php-gd php-xml php-cli php-mbstring php-soap php-xmlrpc php-zip nodejs npm php-mysql
+sudo apt update -y
+sudo apt install -y net-tools php libapache2-mod-php php-gd php-xml php-cli php-mbstring php-soap php-xmlrpc php-zip nodejs npm php-mysql
 
 # LAMPPのインストール
 
 wget https://www.apachefriends.org/xampp-files/8.1.5/xampp-linux-x64-8.1.5-0-installer.run
 chmod +x xampp-linux-x64-8.1.5-0-installer.run
-yes |  ./xampp-linux-x64-8.1.5-0-installer.run
+yes | sudo ./xampp-linux-x64-8.1.5-0-installer.run
 
 # apache2を停止
 
-systemctl disable apache2
-systemctl stop apache2.service
+sudo systemctl disable apache2
+sudo systemctl stop apache2.service
 
 # phpMyAdminを使用可能に
 
-sed -ie 's/Require local/Require all granted/' /opt/lampp/etc/extra/httpd-xampp.conf >/dev/null 2>&1
+sudo sed -ie 's/Require local/Require all granted/' /opt/lampp/etc/extra/httpd-xampp.conf >/dev/null 2>&1
 
 # LAMPP起動時のエラーを解決
 
-sed -ie '/UserPassword daemon <?/,/?>/s|^|#|' /opt/lampp/etc/proftpd.conf >/dev/null 2>&1
+sudo sed -ie '/UserPassword daemon <?/,/?>/s|^|#|' /opt/lampp/etc/proftpd.conf >/dev/null 2>&1
 
 # LAMPPを起動
 
-/opt/lampp/lampp stop
-/opt/lampp/lampp start
-/opt/lampp/lampp restart
+sudo /opt/lampp/lampp stop
+sudo /opt/lampp/lampp start
+sudo /opt/lampp/lampp restart
 
 # データベースの作成
 
 echo "CREATE DATABASE $main_database" | /opt/lampp/bin/mysql -u root
 echo "CREATE DATABASE $keiziban_database" | /opt/lampp/bin/mysql -u root
 
-
 # Composer のインストール
 
 curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-chmod +x /usr/local/bin/composer
+sudo mv composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
 source ~/.bashrc
 
 # node.js関連のインストール
 
-npm install n -g
-n lts
+sudo npm install n -g
+sudo n lts
 
 # リポジトリのクローン
 
-mv Laravel_Forum-B /opt/lampp/htdocs
+sudo mv Laravel_Forum-B /opt/lampp/htdocs
 cd /opt/lampp/htdocs/Laravel_Forum-B
 
 # ドキュメントルート変更
 
-sed -ie 's/DocumentRoot "\/opt\/lampp\/htdocs"/DocumentRoot "\/opt\/lampp\/htdocs\/Laravel_Forum-B\/public"/' /opt/lampp/etc/httpd.conf
-sed -ie 's/<Directory "\/opt\/lampp\/htdocs">/<Directory "\/opt\/lampp\/htdocs\/Laravel_Forum-B\/public">/' /opt/lampp/etc/httpd.conf
+sudo sed -ie 's/DocumentRoot "\/opt\/lampp\/htdocs"/DocumentRoot "\/opt\/lampp\/htdocs\/Laravel_Forum-B\/public"/' /opt/lampp/etc/httpd.conf
+sudo sed -ie 's/<Directory "\/opt\/lampp\/htdocs">/<Directory "\/opt\/lampp\/htdocs\/Laravel_Forum-B\/public">/' /opt/lampp/etc/httpd.conf
 
 # リポジトリの公開準備
 
@@ -89,10 +88,10 @@ sed -ie "s/DB_DATABASE_KEIZIBAN=/DB_DATABASE_KEIZIBAN=$keiziban_database/" .env
 
 chmod 777 -R storage
 
-php artisan migrate
+sudo php artisan migrate
 
 # LAMPP の起動
 
-/opt/lampp/lampp restart
+sudo /opt/lampp/lampp restart
 
 exit 0
