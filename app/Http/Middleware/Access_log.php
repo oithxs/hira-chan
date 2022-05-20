2f26f04484dee4e0076e34750573f4c0c9b6a80f
satou1011 <e1n21041@st.oit.ac.jp>
2022-05-20 09:06:32 +0000

    GET->RECORD,の保護

--- app/Http/Middleware/Access_log.php
+++ app/Http/Middleware/Access_log.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use App\Models\Get_AccessLog;
+use App\Models\Record_AccessLog;
 
 class Access_log
 {
@@ -20,7 +20,7 @@ class Access_log
         $table = $request->thread_id;
         $user = $request->user()->id;
         $ip = $request->ip();
-        $save = new Get_AccessLog;
+        $save = new Record_AccessLog;
         $save->func(
             $table,
             $user,
--- app/Models/Record_AccessLog.php
+++ app/Models/Record_AccessLog.php
@@ -3,8 +3,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-class Get_AccessLog extends Model {
+class Record_AccessLog extends Model {
     public function func($table, $user, $ip) {
+        $table = htmlspecialchars($table,ENT_QUOTES,"UTF-8");
         $sql= <<<EOF
         INSERT INTO
             $table (
