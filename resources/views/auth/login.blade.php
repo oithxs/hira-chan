ed4392620ca1887b8b30a459120004c5db119cfd
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-06-24 20:51:18 +0900

    例のメールアドレス適応

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,8 +37,9 @@
 
             <div class="input-group mt-3">
                 <x-jet-input id="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="text"
-                    name="email" placeholder="（例）e1n11111" aria-label="（例）{{ config('AddConfig.mail.example') }}"
-                    aria-describedby="basic-addon2" required autofocus />
+                    name="email" :value="old('email')" placeholder="（例）{{ config('AddConfig.mail.example') }}"
+                    aria-label="（例）{{ config('AddConfig.mail.example') }}" aria-describedby="basic-addon2" required
+                    autofocus />
                 <span class="input-group-text" id="basic-addon2">@st.oit.ac.jp</span>
                 @if ($errors->has('email'))
                 <span class="invalid-feedback" role="alert">
