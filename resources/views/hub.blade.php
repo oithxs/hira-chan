b586a13a6a4bab71f4e4e2d6591bb924bc473e64
sho037 <103867061+sho037@users.noreply.github.com>
2022-06-24 15:28:21 +0900

    darkmodeの適用を可能にするようにクラスを削除。

--- resources/views/hub.blade.php
+++ resources/views/hub.blade.php
@@ -16,7 +16,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="overflow-hidden shadow-xl sm:rounded-lg">
 
                 <head>
                     <meta charset="utf-8" />
