3b49bf052b691f6e54ad19c39ba9e25ff2d91ae2
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-06-26 00:45:34 +0900

    書き込み欄を分割・ツイッターは配置イメージがつかめなかったのでとりあえずshowに配置

--- resources/views/dashboard/messages.blade.php
+++ resources/views/dashboard/messages.blade.php
@@ -60,27 +60,9 @@
 <!-- ここからスレッドが存在したとき -->
 @if ($result == 1)
 <div class="row">
-    <hr />
-    <div class="col-sm-4 col-xs-12">
-        <a href="/dashboard">トップページへ</a>
-        <form id="dashboard_sendMessage_form">
-            <div class="mb-2">
-                <label class="form-label">コメント</label>
-                <textarea class="form-control" id="dashboard_message_textarea" rows="4"></textarea>
-                <br />
-                <div class="form-text">
-                    入力欄の右下にマウスカーソルを移動させると，高さを変えることができます
-                </div>
-                <div id="dashboard_sendAlertArea"></div>
-            </div>
-        </form>
-        <button id="dashboard_sendMessage_btn" class="btn btn-primary">
-            {{ __("Write forum") }}
-        </button>
-    </div>
 
     <!-- ここから非同期通信で掲示板の表示 -->
-    <div id="dashboard_displayArea" class="col-sm-8 col-xs-12" style="
+    <div id="dashboard_displayArea" class="col-sm-12 col-xs-12" style="
                                     height: 70vh;
                                     width: 100;
                                     overflow-y: scroll;
--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -58,10 +58,36 @@
 
         <!-- ここからスレッド一覧の移動や書き込みなど -->
         <div class="row container">
-            <div class="py-12 col-lg-12 col-md-12">
+            <div class="py-12 col-lg-8 col-md-12">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                        @livewire('dashboard.display-support')
+                        @if ($main_type == 'threads')
+                        @elseif ($main_type == 'messages')
+                        @livewire('dashboard.send-comment')
+                        <!-- elseif (カテゴリ別) -->
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+            <div class="py-12 col-lg-4 col-md-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                        <!--
+                            ここからTwitterの表示
+                            配置のイメージがつかめなかったので応急処置的にここに書いています
+                        -->
+                        @if (Auth::user()->thema == 0)
+                        <a class="twitter-timeline" data-chrome="nofooter" data-width="400" data-height="550"
+                            data-theme="light" href="https://twitter.com/hxs_?ref_src=twsrc%5Etfw">Tweets by
+                            hxs_</a>
+                        @elseif (Auth::user()->thema == 1)
+                        <a class="twitter-timeline" data-chrome="nofooter" data-width="400" data-height="550"
+                            data-theme="dark" href="https://twitter.com/hxs_?ref_src=twsrc%5Etfw">Tweets by hxs_</a>
+                        @endif
+
+                        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                        <!-- ここまでTwitterの表示 -->
                     </div>
                 </div>
             </div>
