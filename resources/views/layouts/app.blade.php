edeca02dd30dda2a36190f478f9356a377f26023
sho037 <103867061+sho037@users.noreply.github.com>
2022-06-26 15:19:40 +0900

    jQueryを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,8 @@
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
 
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
