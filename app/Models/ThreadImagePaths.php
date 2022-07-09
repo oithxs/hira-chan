2cdad881ab69c07b5bb547d9b315887de67185f3
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-07-09 20:42:22 +0900

    画像（パス）保存用テーブルの作成

---
+++ app/Models/ThreadImagePaths.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ThreadImagePaths extends Model
+{
+    use HasFactory;
+
+    /**
+     * Database to be connected
+     *
+     * @var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * Tables to be associated
+     *
+     * @var string
+     */
+    protected $table = 'thread_image_paths';
+
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'thread_id',
+        'message_id',
+        'user_email',
+        'img_path',
+        'img_size',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'thread_id',
+        'img_path',
+        'img_size'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'update_at' => 'datetime',
+    ];
+}
