3f0fee9e4118552a88c7a9916b09f267a99dcd8e
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-07-09 20:22:11 +0900

    データベースに保存する時間のフォーマット？を変更

--- app/Models/UserPageThemas.php
+++ app/Models/UserPageThemas.php
@@ -39,7 +39,7 @@ class UserPageThemas extends Model
      * @var array
      */
     protected $casts = [
-        'created_at' => 'datetime',
-        'update_at' => 'datetime',
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'update_at' => 'datetime:Y-m-d H:i:s',
     ];
 }
