7b16bd31450d715fc2d15d9c12e648e782af8e3d
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-07-21 20:47:58 +0900

    コンソールに表示されていたエラーの解決

--- resources/views/dashboard/threads.blade.php
+++ resources/views/dashboard/threads.blade.php
@@ -102,46 +102,48 @@
             @endforeach
         </tbody>
     </table>
-</div>
 
-<!-- ここからスレッド作成時に表示されるモーダル -->
-<div class="modal fade" id="CreateThread_Modal" tabindex="-1" aria-labelledby="CreateThreadModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-body">
-                <form id="dashboard_create_thread_form">
-                    <label for="thread-name" class="" col-form-label>
-                        {{ __("Thread name") }}
-                    </label>
-                    <input id="dashboard_create_thread_text" type="text" class="form-control">
-                    <label for="thread-category" class="" col-form-label>
-                        {{ __('Thread category') }}
-                    </label>
-                    <select id="dashboard_thread_category_select">
-                        <option value="">選択して下さい</option>
-                        @foreach ($categorys as $category)
-                        <option value="{{ $category->category_name }}">{{ $category->category_name }}</option>
-                        @endforeach
-                    </select>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                    Close
-                </button>
-                <button id="dashboard_create_thread_btn" type="button" class="btn btn-primary" data-bs-dismiss="modal">
-                    Save changes
-                </button>
+    <!-- ここからスレッド作成時に表示されるモーダル -->
+    <div class="modal fade" id="CreateThread_Modal" tabindex="-1" aria-labelledby="CreateThreadModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <form id="dashboard_create_thread_form">
+                        <label for="thread-name" class="" col-form-label>
+                            {{ __("Thread name") }}
+                        </label>
+                        <input id="dashboard_create_thread_text" type="text" class="form-control">
+                        <label for="thread-category" class="" col-form-label>
+                            {{ __('Thread category') }}
+                        </label>
+                        <select id="dashboard_thread_category_select">
+                            <option value="">選択して下さい</option>
+                            @foreach ($categorys as $category)
+                            <option value="{{ $category->category_name }}">{{ $category->category_name }}</option>
+                            @endforeach
+                        </select>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                    <button id="dashboard_create_thread_btn" type="button" class="btn btn-primary"
+                        data-bs-dismiss="modal">
+                        Save changes
+                    </button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<!-- ここまでスレッド作成時に表示されるモーダル -->
+    <!-- ここまでスレッド作成時に表示されるモーダル -->
 
-<!-- ここからデザイン関係なし -->
-<script>
-    const url = "{{ url('') }}";
-</script>
-<script src="{{ asset('js/app_jquery.js') }}"></script>
-<!-- ここまでデザイン関係なし -->
+    <!-- ここからデザイン関係なし -->
+    <script>
+        const url = "{{ url('') }}";
+    </script>
+    <script src="{{ asset('js/app_jquery.js') }}"></script>
+    <!-- ここまでデザイン関係なし -->
+
+</div>
