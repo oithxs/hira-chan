f6f72e000d99847f7c7de560a0bf60b715d6399f
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-08-25 21:36:48 +0900

    Rename requests remove "s"

--- app/Http/Requests/report/FormContactAdministratorRequest.php
+++ app/Http/Requests/report/FormContactAdministratorRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-class FormContactAdministratorsRequest extends FormRequest
+class FormContactAdministratorRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
