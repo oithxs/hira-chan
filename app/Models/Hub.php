289f95fc5b79b76b76af30a54eea9029f0204e79
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-09-01 23:24:47 +0900

    hub.id の型を uuid に変更

--- app/Models/Hub.php
+++ app/Models/Hub.php
@@ -3,9 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Hub extends Model
+class Hub extends UuidModel
 {
     use HasFactory;
 
