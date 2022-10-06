93ea2775ff86020d36327d4f452b240c146a15d1
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-10-06 19:22:15 +0900

    スレッドアクセス時のヘッダー（カテゴリ指定）部分の削除

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -4,13 +4,15 @@
  -->
 
 <x-app-layout>
+    <!-- ここからカテゴリ別 -->
+    @if ($main_type == 'threads')
     <x-slot name="header">
         <h2 class="font-semibold text-xl leading-tight">
-            <!-- ここからカテゴリ別 -->
             @livewire('dashboard.header')
-            <!-- ここまでカテゴリ別 -->
         </h2>
     </x-slot>
+    @endif
+    <!-- ここまでカテゴリ別 -->
 
     <div class="container">
         <div class="row">
@@ -65,12 +67,10 @@
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-2">
                     <!-- ここからTwitterの表示 -->
                     @if (!Auth::check() || Auth::user()->thema == 0)
-                    <a class="twitter-timeline" data-chrome="nofooter" data-width="400" data-height="550"
-                        data-theme="light" href="https://twitter.com/hxs_?ref_src=twsrc%5Etfw">Tweets by
+                    <a class="twitter-timeline" data-chrome="nofooter" data-width="400" data-height="550" data-theme="light" href="https://twitter.com/hxs_?ref_src=twsrc%5Etfw">Tweets by
                         hxs_</a>
                     @elseif (Auth::user()->thema == 1)
-                    <a class="twitter-timeline" data-chrome="nofooter" data-width="400" data-height="550"
-                        data-theme="dark" href="https://twitter.com/hxs_?ref_src=twsrc%5Etfw">Tweets by hxs_</a>
+                    <a class="twitter-timeline" data-chrome="nofooter" data-width="400" data-height="550" data-theme="dark" href="https://twitter.com/hxs_?ref_src=twsrc%5Etfw">Tweets by hxs_</a>
                     @endif
                     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                     <!-- ここまでTwitterの表示 -->
