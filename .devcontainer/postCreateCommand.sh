a720528beeeb30040f7062f8bcba90858cb01ffa
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-10-27 00:20:53 +0900

    devcontainerで自動実行（Setup）されるスクリプトの追加

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cp /usr/src/app/.env.example /usr/src/app/.env
+
+mkdir storage/app/public/images
+mkdir storage/app/public/images/thread_message
+chmod -R 777 storage
+chmod -R 777 bootstrap/cache
+chmod -R 777 public/uploads
+
+composer install
+npm install --update-binary --no-shrinkwrap
+php artisan key:generate
+php artisan migrate
+php artisan admin:install
+php artisan db:seed
+php artisan admin:import helpers
+php artisan admin:import log-viewer
+php artisan storage:link
+
+sed -ie "s/DB_HOST=127.0.0.1/DB_HOST=hira-chan_mysql/g" /usr/src/app/.env
+sed -ie "s/DB_DATABASE=/DB_DATABASE=forum/g" /usr/src/app/.env
+sed -ie "s/DB_PASSWORD=/DB_PASSWORD=rootpass/g" /usr/src/app/.env
+
+exit 0
