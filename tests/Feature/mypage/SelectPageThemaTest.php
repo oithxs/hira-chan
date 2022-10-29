9b1bada57659f4167a34a66851ce2c2fbd2b1331
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-10-29 12:44:34 +0000

    テストでseederが実行される様に変更・スペルミスを修正

--- tests/Feature/mypage/SelectPageThemaTest.php
+++ tests/Feature/mypage/SelectPageThemaTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class SelectPageThemaTest extends TestCase
+class SelectPagethemeTest extends TestCase
 {
     use RefreshDatabase;
 
@@ -19,57 +19,57 @@ class SelectPageThemaTest extends TestCase
         $this->user = User::factory()->create();
     }
 
-    public function test_not_login_get_access_select_page_thema()
+    public function test_not_login_get_access_select_page_theme()
     {
-        $response = $this->get('/jQuery.ajax/page_thema');
+        $response = $this->get('/jQuery.ajax/page_theme');
 
         $response->assertStatus(404);
     }
 
-    public function test_user_get_access_select_page_thema()
+    public function test_user_get_access_select_page_theme()
     {
         $response = $this
             ->actingAs($this->user)
-            ->get('/jQuery.ajax/page_thema');
+            ->get('/jQuery.ajax/page_theme');
 
         $response->assertStatus(404);
     }
 
-    public function test_not_login_post_access_select_default_page_thema()
+    public function test_not_login_post_access_select_default_page_theme()
     {
-        $response = $this->post('/jQuery.ajax/page_thema', [
-            'page_thema' => 0,
+        $response = $this->post('/jQuery.ajax/page_theme', [
+            'page_theme' => 1,
         ]);
 
         $response->assertRedirect('/login');
     }
 
-    public function test_user_post_access_select_default_page_thema()
+    public function test_user_post_access_select_default_page_theme()
     {
         $response = $this
             ->actingAs($this->user)
-            ->post('/jQuery.ajax/page_thema', [
-                'page_thema' => 0,
+            ->post('/jQuery.ajax/page_theme', [
+                'page_theme' => 1,
             ]);
 
         $response->assertStatus(200);
     }
 
-    public function test_not_login_post_access_select_dark_page_thema()
+    public function test_not_login_post_access_select_dark_page_theme()
     {
-        $response = $this->post('/jQuery.ajax/page_thema', [
-            'page_thema' => 1,
+        $response = $this->post('/jQuery.ajax/page_theme', [
+            'page_theme' => 2,
         ]);
 
         $response->assertRedirect('/login');
     }
 
-    public function test_user_post_access_select_dark_page_thema()
+    public function test_user_post_access_select_dark_page_theme()
     {
         $response = $this
             ->actingAs($this->user)
-            ->post('/jQuery.ajax/page_thema', [
-                'page_thema' => 1,
+            ->post('/jQuery.ajax/page_theme', [
+                'page_theme' => 2,
             ]);
 
         $response->assertStatus(200);
