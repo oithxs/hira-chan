e9fb6f30353442c1d9df3e3b3d5768295b181ce1
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-11-27 20:46:45 +0900

    FormRequest（バリデーション）を使うテストに使用するスーパークラスの作成

---
+++ tests/UseFormRequestTestCase.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests;
+
+use Exception;
+
+abstract class UseFormRequestTestCase extends TestCase
+{
+    /**
+     * Methods to test.
+     *
+     * @var array
+     */
+    protected array $method;
+
+    /**
+     * Method Arguments to be Tested.
+     *
+     * @var array
+     */
+    protected array $args;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->setMethod();
+        $this->setArgument();
+    }
+
+    /**
+     * Set the target method in the child class.
+     *
+     * @return void
+     */
+    abstract protected function setMethod(): void;
+
+    /**
+     * Set the arguments of the target method in the child class.
+     *
+     * @return void
+     */
+    abstract protected function setArgument(): void;
+
+    /**
+     * Called first when the useFormRequest method is executed
+     *
+     * @return void
+     */
+    protected function reserve(): void
+    {
+        // Can be defined in a child class
+    }
+
+    /**
+     * Changes the arguments when the target method is executed.
+     * The method returns whether the target method can be executed or not.
+     *
+     * @param array $key
+     * @param array $value
+     * @return void
+     */
+    protected function useFormRequest(array $keys, array $values)
+    {
+        $this->reserve();
+        for ($i = 0; $i < count($keys); $i++) {
+            $this->args[$keys[$i]] = $values[$i];
+        }
+        try {
+            ($this->method)($this->args);
+        } catch (Exception $e) {
+            return false;
+        }
+        return true;
+    }
+}
