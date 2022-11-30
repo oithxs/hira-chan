d91a0a6ed49f85ed473b29c749cd3ef6e3c6a792
sho037 <103867061+sho037@users.noreply.github.com>
2022-11-30 18:44:38 +0900

    コンテンツ内の背景色を変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
                 <p class="h1">HxS コンピュータ部</p>
             </div>
 
-            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+            <div class="mt-8 overflow-hidden shadow sm:rounded-lg">
                 <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                         <div class="flex items-center">
