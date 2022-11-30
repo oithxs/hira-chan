3575848394cbea109f4df3031fde7f5fce860276
sho037 <103867061+sho037@users.noreply.github.com>
2022-11-30 18:44:10 +0900

    ウェルカムページの背景色

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
 
 <body class="antialiased">
     <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        class="relative flex items-top justify-center min-h-screen sm:items-center py-4 sm:pt-0 bg-white">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                 <p class="h1">HxS コンピュータ部</p>
