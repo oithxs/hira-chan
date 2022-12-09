0ab21c2ed7341cd9ece638415d85d84f9c978462
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-12-09 23:13:51 +0900

    ユーザプロフィール更新時に呼び出されるメソッドのテスト作成

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -10,7 +10,14 @@ use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
 class UpdateUserProfileInformation implements UpdatesUserProfileInformation
 {
     /**
-     * Validate and update the given user's profile information.
+     * 指定されたユーザーのプロファイル情報を検証し，更新する．
+     *
+     * 'email'は変更を禁止している
+     * 'photo'はそもそも不明
+     *
+     * @link https://jetstream.laravel.com/2.x/features/profile-management.html
+     * @link https://readouble.com/laravel/9.x/ja/validation.html
+     * @link https://readouble.com/laravel/9.x/ja/queries.html
      *
      * @param  mixed  $user
      * @param  array  $input
@@ -42,7 +49,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     }
 
     /**
-     * Update the given verified user's profile information.
+     * 指定された認証ユーザーのプロファイル情報を更新する．
      *
      * @param  mixed  $user
      * @param  array  $input
