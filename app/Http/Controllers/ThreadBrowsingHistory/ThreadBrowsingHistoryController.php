ec36d652a632f136a35b2411d6eb287c889e2bec
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-01-09 23:36:35 +0900

    refactor: 閲覧履歴ページ表示Controllerの処理分割

--- app/Http/Controllers/ThreadBrowsingHistory/ThreadBrowsingHistoryController.php
+++ app/Http/Controllers/ThreadBrowsingHistory/ThreadBrowsingHistoryController.php
@@ -5,19 +5,34 @@ namespace App\Http\Controllers\ThreadBrowsingHistory;
 use App\Http\Controllers\Controller;
 use App\Models\AccessLog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\View\View;
 
 class ThreadBrowsingHistoryController extends Controller
 {
     /**
-     * 重複なしで，過去に閲覧したスレッドを取得する
+     * 閲覧履歴ページを表示する
      *
      * @param Request $request
-     * @return void
+     * @return View
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): View
     {
-        $history = AccessLog::with('hub')
-            ->where('user_id', '=', $request->user()->id)
+        return view('thread-browsing-history.show', [
+            'history' => $this->getHistory($request->user()->id),
+        ]);
+    }
+
+    /**
+     * 重複なしで，過去に閲覧したスレッドを取得する
+     *
+     * @param string $user_id
+     * @return Collection
+     */
+    private function getHistory(string $user_id): Collection
+    {
+        return  AccessLog::with('hub')
+            ->where('user_id', '=', $user_id)
             ->whereIn('id', function ($query) {
                 $query->selectRaw('MAX(id)')
                     ->from('access_logs')
@@ -25,9 +40,5 @@ class ThreadBrowsingHistoryController extends Controller
             })
             ->orderByDesc('id')
             ->get();
-
-        return view('thread-browsing-history.show', [
-            'history' => $history,
-        ]);
     }
 }
