c8cba7f30e15f98f80bc1ab91444ded2b596b2cd
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-01-12 22:43:47 +0900

    feat: htmlの特殊文字変換を行うServiceの作成

---
+++ app/Services/Support/HtmlSpecialCharsService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services\Support;
+
+class HtmlSpecialCharsService
+{
+    /**
+     * 変換元 key
+     * 変換後 value
+     *
+     * とした連想配列
+     */
+    const HTML_SPECIAL_CHARACTER = [
+        '&' => '&amp;',
+        '<' => '&lt;',
+        '>' => '&gt;',
+        ' ' => '&ensp;',
+        '　' => '&emsp;',
+        "\n" => '<br>',
+        "\t" => '&ensp;&ensp;'
+    ];
+
+    /**
+     * 元の文字列と同様にHTMLで表示できるように文字列を変換する
+     *
+     * @param string $text
+     * @return string
+     */
+    public static function encode(string $text): string
+    {
+        foreach (self::HTML_SPECIAL_CHARACTER as $key => $value) {
+            $text = str_replace($key, $value, $text);
+        }
+        return $text;
+    }
+}
