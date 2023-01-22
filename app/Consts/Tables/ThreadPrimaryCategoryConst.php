2dc9b5038e33293f2ab9aa1e1a931bc53f288144
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-01-22 22:57:54 +0900

    feat: thread_primary_category テーブル関連の定数にカラムの情報を追加

--- app/Consts/Tables/ThreadPrimaryCategoryConst.php
+++ app/Consts/Tables/ThreadPrimaryCategoryConst.php
@@ -4,6 +4,23 @@ namespace App\Consts\Tables;
 
 class ThreadPrimaryCategoryConst
 {
+    // テーブル名
+    const NAME = 'thread_primary_categorys';
+
+    // カラム名
+    const ID = 'id';
+    const _NAME = 'name';
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    // カラム一覧
+    const COLUMNS = [
+        self::ID,
+        self::_NAME,
+        self::CREATED_AT,
+        self::UPDATED_AT,
+    ];
+
     // php artisan db:seed を実行後に保存されるデータ
     const CLUB = '部活';
     const COLLEGE_YEAR = '学年';
