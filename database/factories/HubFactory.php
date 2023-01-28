f7b5cd2d12ef475aebe57291a80349bdf1c8177e
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-01-29 02:04:22 +0900

    feat: hub テーブルのファクトリに大枠カテゴリを指定するメソッドを追加

--- database/factories/HubFactory.php
+++ database/factories/HubFactory.php
@@ -3,8 +3,10 @@
 namespace Database\Factories;
 
 use App\Models\Hub;
+use App\Models\ThreadPrimaryCategory;
 use App\Models\ThreadSecondaryCategory;
 use App\Models\User;
+use App\Repositories\ThreadPrimaryCategoryRepository;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -38,5 +40,28 @@ class HubFactory extends Factory
             'user_id' => User::factory()->create()->id,
             'name' => $this->faker->name(),
         ];
+    }
+
+    /**
+     * 大枠カテゴリを指定する
+     *
+     * @param string $primaryCategory
+     * @return HubFactory
+     */
+    public function primaryCategory(string $primaryCategory): HubFactory
+    {
+        return $this->state(function (array $attributes) use ($primaryCategory) {
+            return [
+                'thread_secondary_category_id' => random_int(
+                    ThreadPrimaryCategoryRepository::getThreadSecondaryCategoryHasMany($primaryCategory)
+                        ->first()
+                        ->id,
+                    ThreadPrimaryCategoryRepository::getThreadSecondaryCategoryHasMany($primaryCategory)
+                        ->orderByDesc('id')
+                        ->first()
+                        ->id
+                ),
+            ];
+        });
     }
 }
