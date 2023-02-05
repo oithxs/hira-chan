078886571a9f1535fe80785bee243f6f43c89112
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-02-05 22:23:22 +0900

    test: likeテーブル関連のテストでassertSameを使用する際によく使うメソッドをまとめたトレイトを作成

---
+++ tests/Support/AssertSame/Tables/LikeTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Support\AssertSame\Tables;
+
+use App\Consts\Tables\LikeConst;
+use Tests\Support\ArrayToolsTrait;
+
+trait LikeTrait
+{
+    use ArrayToolsTrait;
+
+    /**
+     * @var array 実際のlikeテーブルのデータ
+     */
+    public array $like;
+
+    /**
+     * likes テーブルのすべてのカラム名を期待する値として取得する
+     *
+     * @return array hubsテーブルのすべてのカラム名
+     */
+    public function getKeysExpected(): array
+    {
+        return LikeConst::COLUMNS;
+    }
+
+    /**
+     * likes テーブルの実際の値を取得する
+     *
+     * @param array|null $args
+     * @return array
+     */
+    public function getValuesActual(array $args = null): array
+    {
+        return $this->getArrayElement($this->like, [
+            LikeConst::CLUB_THREAD_ID,
+            LikeConst::COLLEGE_YEAR_THREAD_ID,
+            LikeConst::DEPARTMENT_THREAD_ID,
+            LikeConst::JOB_HUNTING_THREAD_ID,
+            LikeConst::LECTURE_THREAD_ID,
+            LikeConst::USER_ID,
+        ]);
+    }
+}
