417707d62fa66e68030bd3140fb62e4006b569fd
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-02-07 21:57:26 +0900

    test: 書き込みへいいねを行い，書き込みとそれに対するいいねをメンバ変数に代入するトレイトの作成

---
+++ tests/Support/LikeTestTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Support;
+
+use App\Consts\Tables\ThreadsConst;
+use App\Models\Like;
+use App\Models\ThreadModel;
+
+trait LikeTestTrait
+{
+    /**
+     * @var array スレッドへの書き込み
+     */
+    public array $posts;
+
+    /**
+     * @var array 書き込みへのいいね
+     */
+    public array $likes;
+
+    /**
+     * すべての大枠カテゴリの書き込みをメンバ変数に代入し，
+     * すべての書き込みにいいねをする
+     *
+     * @param integer $num
+     * @return void
+     */
+    public function likeSetUp(int $num = 10): void
+    {
+        $this->posts = [];
+        for ($i = 0; $i < count(ThreadsConst::MODEL_FQCNS); $i++) {
+            $this->posts[$i] = ThreadsConst::MODEL_FQCNS[$i]::factory()->create();
+            for ($j = 0; $j < $num; $j++) {
+                $this->likes[$i][$j] = Like::factory()->post($this->posts[$i])->create();
+            }
+        }
+    }
+}
