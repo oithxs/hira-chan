918a4342d6b6926761f853de10c11e51551141e0
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-02-08 12:41:13 +0900

    feat: user_page_themesテーブル関連の定数クラスに初期データの情報を追加

--- app/Consts/Tables/UserPageThemeConst.php
+++ app/Consts/Tables/UserPageThemeConst.php
@@ -9,4 +9,14 @@ class UserPageThemeConst
 
     // 外部キーとして利用されるときのカラム名
     const USED_FOREIGN_KEY = 'user_page_theme_id';
+
+    // php artisan db:seed を実行後に保存されるデータ
+    const DEFAULT = 'default';
+    const DARK = 'dark';
+
+    // 保存されたページテーマ一覧
+    const PAGE_THEMES = [
+        self::DEFAULT,
+        self::DARK,
+    ];
 }
