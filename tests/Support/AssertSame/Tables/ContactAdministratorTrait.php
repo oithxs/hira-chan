8db019c91dd6a596cdd741825c84a658d2b9005c
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-02-21 01:02:51 +0900

    test: contact_administrators テーブルのデータを assertSame で検証する際によく使用するメソッドをまとめたトレイトを作成

---
+++ tests/Support/AssertSame/Tables/ContactAdministratorTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Support\AssertSame\Tables;
+
+use App\Consts\Tables\ContactAdministratorConst;
+use Tests\Support\ArrayToolsTrait;
+
+trait ContactAdministratorTrait
+{
+    use ArrayToolsTrait;
+
+    /**
+     * @var array 期待する contact_administrators テーブルのデータ
+     */
+    public array $contactAdministrator;
+
+    /**
+     * contact_administrators テーブルすべてのカラム名を期待する値として取得する
+     *
+     * @return array contact_administrators テーブルのすべてのカラム名
+     */
+    public function getKeysExpected(): array
+    {
+        return ContactAdministratorConst::COLUMNS;
+    }
+
+    /**
+     * contact_administrators テーブルの一部のカラムのデータを実際の値として取得する
+     *
+     * @param array|null $args 引数は使用しない
+     * @return array contact_administrators テーブルの一部のカラムのデータ
+     */
+    public function getValuesActual(array $args = null): array
+    {
+        return $this->getArrayElement($this->contactAdministrator, [
+            'user_id',
+            'type',
+            'message',
+        ]);
+    }
+}
