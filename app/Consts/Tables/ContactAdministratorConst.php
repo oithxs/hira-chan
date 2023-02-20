3be6bcab165df6e2004eeebe04e185cf2e93fc59
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-02-21 00:55:46 +0900

    feat: contact_administrators テーブル関連の定数クラスにカラムの情報を追加

--- app/Consts/Tables/ContactAdministratorConst.php
+++ app/Consts/Tables/ContactAdministratorConst.php
@@ -9,4 +9,22 @@ class ContactAdministratorConst
 
     // 外部キーとして利用されるときのカラム名
     const USED_FOREIGN_KEY = 'contact_administrator_id';
+
+    // カラム名
+    const ID = 'id';
+    const USER_ID = UserConst::USED_FOREIGN_KEY;
+    const TYPE = 'type';
+    const MESSAGE = 'message';
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    // カラム一覧
+    const COLUMNS = [
+        self::ID,
+        self::USER_ID,
+        self::TYPE,
+        self::MESSAGE,
+        self::CREATED_AT,
+        self::UPDATED_AT,
+    ];
 }
