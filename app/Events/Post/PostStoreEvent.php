185fd22a6ebcc8241939d2a66d3257c49f7b9c61
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-03-25 21:49:00 +0900

    refactor: チャンネル名を保存する定数クラスを作成

--- app/Events/Post/PostStoreEvent.php
+++ app/Events/Post/PostStoreEvent.php
@@ -2,20 +2,22 @@
 
 namespace App\Events\Post;
 
+use App\Consts\ChannelConst;
 use App\Http\Resources\PostResource;
-use App\Models\Hub;
 use App\Models\ThreadModel;
-use Error;
-use Exception;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Http\Request;
 use Illuminate\Queue\SerializesModels;
 
+/**
+ * スレッドに書き込みが行われた際に発生するイベント
+ *
+ * スレッドに追加された書き込みを受け取る
+ */
 class PostStoreEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
@@ -25,7 +27,7 @@ class PostStoreEvent implements ShouldBroadcast
     private PostResource $response;
 
     /**
-     * 新しいイベントインスタンスを作成します.
+     * 新しいイベントインスタンスを作成する
      */
     public function __construct(string $threadId, ThreadModel $post)
     {
@@ -34,21 +36,23 @@ class PostStoreEvent implements ShouldBroadcast
     }
 
     /**
-     * イベントが放送されるべきチャンネルを取得します
+     * イベントが放送されるべきチャンネルを取得する
+     *
+     * チャンネル名 => threadBrowsing.<スレッドID>
      *
      * @return array<int, \Illuminate\Broadcasting\Channel>
      */
     public function broadcastOn(): array
     {
         return [
-            new Channel('threadBrowsing.' . $this->threadId),
+            new Channel(ChannelConst::THREAD_BROWSING . $this->threadId),
         ];
     }
 
     /**
      * ブロードキャストされるデータを取得する
      *
-     * @return
+     * @return array 追加された書き込み
      */
     public function broadcastWith()
     {
