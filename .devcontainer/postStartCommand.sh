4ea4664e099ada27516ede4913fff67943ad4f0d
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-05-11 16:51:14 +0900

    chore: 使用するイメージの更新・開発環境をより本番環境に近くなるように変更

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+# 各種サービス起動
+sudo su <<EOF
+service postfix start
+service nginx start
+service php8.1-fpm start
+EOF
+
+# 使用できないセッションの削除
 screen -wipe
 
+# セッションの作成
 screen -dmS queue
 screen -dmS build
 
+# queue セッションで Laravel のキューを動作
 screen -S queue -X stuff ' \
     cd "$WORKDIR"; \
     php artisan queue:work \
 \n'
 
+# build セッションで webpack を用いた自動ビルドを動作
 screen -S build -X stuff ' \
     cd "$WORKDIR"; \
     npm run watch \
